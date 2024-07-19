78e7102a7d28dfc7b52ed25b6faac69f6b44d94b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-19 18:37:30 +0900

    ✅ [add] BaseEntity
    CreatedAt, UpdatedAt 베이스 엔티티 작성

--- src/main/java/com/winner_cat/WinnercatApplication.java
+++ src/main/java/com/winner_cat/WinnercatApplication.java
@@ -2,8 +2,10 @@ package com.winner_cat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WinnercatApplication {
 
 	public static void main(String[] args) {
