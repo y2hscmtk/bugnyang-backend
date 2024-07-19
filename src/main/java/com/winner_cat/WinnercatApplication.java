b8841c6ef9e101b1243decbb50c48e8821e89f41
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-19 17:13:42 +0900

    🎉 프로젝트 생성

---
+++ src/main/java/com/winner_cat/WinnercatApplication.java
@@ -0,0 +1,13 @@
+package com.winner_cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinnercatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinnercatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winner_cat/WinnercatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winner_cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinnercatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
