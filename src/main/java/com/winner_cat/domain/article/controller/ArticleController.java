d3a9592d2c6fd7edc16c9566d6710d4168b070ad
kimjungeun <lucycy255@gmail.com>
2024-07-21 15:32:28 +0900

    ✅ [add] ArticleController 작성

---
+++ src/main/java/com/winner_cat/domain/article/controller/ArticleController.java
@@ -0,0 +1,25 @@
+package com.winner_cat.domain.article.controller;
+
+import com.winner_cat.domain.article.service.ArticleService;
+import com.winner_cat.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/article")
+@RequiredArgsConstructor
+public class ArticleController {
+    private final ArticleService articleService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<?>> createArticle(
+            @Valid @RequestBody ArticleCreateDto.Req req) {
+        ResponseEntity<ApiResponse<?>> result = articleService.createArticle(req);
+        return result;
+    }
+}
