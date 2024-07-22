a69b24ae5ba1d6d43e1006e87912e7fe023b4181
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-23 01:54:27 +0900

    ✏️ [fix] JWT 쿠키 HttpOnly false로 수정

--- src/main/java/com/winner_cat/global/oauth2/handler/CustomSuccessHandler.java
+++ src/main/java/com/winner_cat/global/oauth2/handler/CustomSuccessHandler.java
@@ -60,7 +60,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         cookie.setMaxAge(60 * 60 * 60);
         // cookie.setSecure(true);
         cookie.setPath("/");
-        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(false);
         return cookie;
     }
 }
