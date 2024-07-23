dd7b5d1a00cff54ca2b7adb39a58892521e6c4e5
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-23 20:55:00 +0900

    ✨ [feat] 내가 스크랩한 게시글 보기 서비스 코드 작성

--- src/main/java/com/winner_cat/domain/scrap/repository/ScrapRepository.java
+++ src/main/java/com/winner_cat/domain/scrap/repository/ScrapRepository.java
@@ -1,7 +1,11 @@
 package com.winner_cat.domain.scrap.repository;
 
+import com.winner_cat.domain.member.entity.Member;
 import com.winner_cat.domain.scrap.entity.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+    List<Scrap> findByMember(Member member);
 }
