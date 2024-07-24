806d02013012a1a5df1bc50eb569332261d6dd33
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-24 17:04:16 +0900

    ✨ [feat] 전체 게시글 조회 - 미리보기 Controller 코드 작성

--- src/main/java/com/winner_cat/domain/article/service/ArticleServiceImpl.java
+++ src/main/java/com/winner_cat/domain/article/service/ArticleServiceImpl.java
@@ -210,6 +210,6 @@ public class ArticleServiceImpl implements ArticleService{
                     .build();
             resultDtoList.add(resultDto);
         }
-        return ResponseEntity.ok().body(resultDtoList);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(resultDtoList));
     }
 }
