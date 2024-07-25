4118b7b319b3d1a01a8520c78a2e10d19d7c4b07
kimjungeun <lucycy255@gmail.com>
2024-07-25 18:29:41 +0900

    ✨ [feat] 질문방 미리보기 기능 구현

--- src/main/java/com/winner_cat/domain/questionroom/dto/QuestionRoomListDto.java
+++ src/main/java/com/winner_cat/domain/questionroom/dto/QuestionRoomListDto.java
@@ -1,2 +1,18 @@
-package com.winner_cat.domain.questionroom.dto;public class QuestionRoomListDto {
+package com.winner_cat.domain.questionroom.dto;
+
+import com.winner_cat.domain.questionroom.entity.enums.QuestionState;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class QuestionRoomListDto {
+    @Getter @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class QuestionRoomResponse {
+        private String roomName;
+        private QuestionState state;
+        private LocalDateTime updatedAt;
+    }
 }
--- src/main/java/com/winner_cat/domain/questionroom/repository/QuestionRoomRepository.java
+++ src/main/java/com/winner_cat/domain/questionroom/repository/QuestionRoomRepository.java
@@ -1,7 +1,11 @@
 package com.winner_cat.domain.questionroom.repository;
 
+import com.winner_cat.domain.member.entity.Member;
 import com.winner_cat.domain.questionroom.entity.QuestionRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRoomRepository extends JpaRepository<QuestionRoom,Long> {
+    List<QuestionRoom> findQuestionRoomByMember(Member member);
 }
