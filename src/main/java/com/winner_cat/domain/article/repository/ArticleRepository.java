1cdd7e31d5907ee54a9439eafdc8ab763698b8ba
jungeun-e <lucycy255@gmail.com>
2024-07-29 20:55:24 +0900

    🎨 [chore] 최근 게시글부터 조회하기

--- src/main/java/com/winner_cat/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/winner_cat/domain/article/repository/ArticleRepository.java
@@ -6,11 +6,20 @@ import com.winner_cat.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Page<Article> findByAuthor(Member author, Pageable pageable);
+    @Query("SELECT a FROM Article a WHERE a.author = :author ORDER BY a.createdAt DESC")
+    Page<Article> findByAuthor(@Param("author") Member author, Pageable pageable);
+
+    @Query("SELECT a FROM Article a ORDER BY a.createdAt DESC")
+    Page<Article> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    @Query("SELECT a FROM Article a JOIN a.tags t WHERE t.tag.tagName = :tagName ORDER BY a.createdAt DESC")
+    Page<Article> findByTagName(@Param("tagName") String tagName, Pageable pageable);
 }
