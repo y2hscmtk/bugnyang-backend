12f6b0e3dd95192498040cf3cfb58c18cc3aad2e
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-08-01 18:03:28 +0900

    ✨ [feat] ArticleRepository 오늘 작성된 게시글 찾기 함수 작성

--- src/main/java/com/winner_cat/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/winner_cat/domain/article/repository/ArticleRepository.java
@@ -10,10 +10,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     @Query("SELECT a FROM Article a WHERE a.author = :author ORDER BY a.createdAt DESC")
     Page<Article> findByAuthor(@Param("author") Member author, Pageable pageable);
+
+    // 오늘 작성된 게시글 찾기
+    @Query("SELECT a FROM Article a WHERE a.createdAt >= :startTime AND a.createdAt <= :endTime")
+    List<Article> findAllByCreatedAtBetween(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
 }
