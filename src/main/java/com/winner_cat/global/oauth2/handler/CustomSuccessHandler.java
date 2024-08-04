d3d16cd346d9c4c6d9d073a43c2ab34364ac721a
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-08-04 17:44:50 +0900

    🚑️ [fix] jwt 쿠키 설정 변경

--- src/main/java/com/winner_cat/global/oauth2/handler/CustomSuccessHandler.java
+++ src/main/java/com/winner_cat/global/oauth2/handler/CustomSuccessHandler.java
@@ -61,7 +61,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private Cookie createCookie(String key, String value) {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(60 * 60 * 60);
-        // cookie.setSecure(true);
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setHttpOnly(false);
         return cookie;
